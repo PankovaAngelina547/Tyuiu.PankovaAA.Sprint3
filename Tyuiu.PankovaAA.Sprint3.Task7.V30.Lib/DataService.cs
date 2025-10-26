9137454bd8b487724f2b2dfa3481987f7bd115c1
Ангелина Панькова <Tyuiu.PankovaAA@outlook.com>
2025-10-26 17:05:02 +0500

    Task 7

---
+++ Tyuiu.PankovaAA.Sprint3.Task7.V30.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.PankovaAA.Sprint3.Task7.V30.Lib
+{
+    public class DataService : ISprint3Task7V30
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray; 
+            int len = (stopValue - startValue) + 1; 
+            valueArray = new double[len]; 
+            double y;
+            int count = 0;
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double denominator = Math.Sin(x) + 2;
+
+                if (Math.Abs(denominator) < 0.0001)
+                {
+                    y = 0;
+                }
+                else
+                {
+                    y = (5 * x + 2.5) / denominator + 2 * x + 2;
+                }
+
+                y = Math.Round(y, 2); 
+                valueArray[count] = y; 
+                count++;
+            }
+
+            return valueArray;
+
+        }
+    }
+}
---
+++ Tyuiu.PankovaAA.Sprint3.Task7.V30/Program.cs
@@ -0,0 +1,54 @@
+﻿using Tyuiu.PankovaAA.Sprint3.Task7.V30.Lib;
+namespace Tyuiu.PankovaAA.Sprint3.Task7.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 |Выполнила: Панькова А.А.| ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  Спринт #3                                                              *");
+            Console.WriteLine("*  Тема: добавление к решению иоговых проектов по спринту                 *");
+            Console.WriteLine("*  Задание #07                                                            *");
+            Console.WriteLine("*  Вариант #30                                                            *");
+            Console.WriteLine("*  Выполнила: Панькова Ангелина Алексеевна    |ПИНб-25-1|                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  РЕЗУЛЬТАТ:                                                             *");
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}   | {1,6:f2} |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.ReadKey();
+
+        }
+
+    }
+}
